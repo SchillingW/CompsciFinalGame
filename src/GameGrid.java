e329a661e66b3d8f61af7fac34be79abe292d8da
LiamLime <wschilling003@gmail.com>
2022-05-10 09:16:55 -0400

    comment Main and GameGrid for Andres
    put processing in project

--- src/GameGrid.java
+++ src/GameGrid.java
@@ -1,58 +1,102 @@
 import processing.core.PImage;
 import java.util.ArrayList;
 
+// grid object for this game
 public class GameGrid extends Grid {
 
+    // player object reference
     public final Player player;
+
+    // list ob blocks existing in grid
     public final ArrayList<Block> blocks;
 
+    // timer device for block spawning
     private final StepDevice spawnTimer;
 
+    // template object for spawning blocks
     private final Block blockTemplate;
 
+    // randomness rate for spawning blocks in row
     public final double blockSpawnRate;
 
+    // initialize game grid
     public GameGrid(
             Vector gridSize, long spawnSteps, double blockSpawnRate, int cellSize,
             PImage tileSprite, Player playerTemplate, Block blockTemplate) {
 
+        // initialize grid object
         super(gridSize, cellSize, tileSprite);
+
+        // initialize empty block list
         blocks = new ArrayList<>();
+
+        // initialize player to this grid from template
         player = playerTemplate.asTemplate(this);
+
+        // initialize timer device
         spawnTimer = new StepDevice(spawnSteps);
+
+        // store settings
         this.blockTemplate = blockTemplate;
         this.blockSpawnRate = blockSpawnRate;
     }
 
+    // get any generic object existing in grid
     @Override
     public GridObject getObject(int index) {
+
+        // if trying to get first object give player
         if (index == 0) return player;
+
+        // otherwise return next block object
         return blocks.get(index - 1);
     }
 
+    // get number of generic objects in grid
     @Override
     public int getObjectCount() {
+
+        // number of blocks plus one player
         return blocks.size() + 1;
     }
 
+    // called on game step
     @Override
     public void step() {
+
+        // call grid step
         super.step();
+
+        // step in time for spawn timer
         if (spawnTimer.addInterval(1)) spawn();
     }
 
+    // spawn row of blocks in grid
     public void spawn() {
+
+        // loop through grid columns
         for (int i = 0; i < gridSize.x; i++) {
+
+            // randomly decide whether block should spawn here
             if (Math.random() < blockSpawnRate) {
+
+                // create new block from template and add to grid
                 blocks.add(blockTemplate.asTemplate(i, this));
             }
         }
     }
 
+    // get block at position in grid
     public Block getBlockAt(Vector cell) {
+
+        // iterate through blocks
         for (Block block : blocks) {
+
+            // if position matches return block
             if (cell.equals(block.getPosition())) return block;
         }
+
+        // if no block found return null
         return null;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,28 @@
 import processing.core.PApplet;
 import processing.core.PImage;
 
+// block dropping game with processing library
 public class Main extends PApplet {
 
+    // reference to game grid
     private GameGrid grid;
+
+    // input mapping for movement directions
     private MultiKeyMap<Character, Vector> dpadMapping;
+
+    // timer device for game steps
     private StepDevice stepDevice;
 
+    // run application
     public static void main(String[] args) {
         PApplet.main("Main");
     }
 
+    // called on application start
     @Override
     public void settings() {
 
+        // map input keys to movement directions
         dpadMapping = new MultiKeyMap<>(
                 new Character[][] {
                         new Character[] {'d'},
@@ -29,41 +38,64 @@ public class Main extends PApplet {
                 }
         );
 
-        PImage playerRightSprite = loadImage("art\\Player.png");
-        PImage playerLeftSprite = loadImage("art\\Player.png");
+        // initialize sprite images
+        PImage playerSprite = loadImage("art\\Player.png");
         PImage blockSprite = loadImage("art\\Block.png");
         PImage tileSprite = loadImage("art\\Tile.png");
 
+        // create templates with settings for player and block objects to be created
         Block blockTemplate = new Block(2, blockSprite);
-        Player playerTemplate = new Player(false, 1, playerRightSprite, playerLeftSprite);
+        Player playerTemplate = new Player(false, 1, playerSprite, playerSprite);
 
+        // initialize game grid object with gameplay settings
         grid = new GameGrid(
                 new Vector(9, 11), 16, 0.5, 40,
                 tileSprite, playerTemplate, blockTemplate);
 
+        // set window dimensions based on grid size
         Vector windowSize = grid.cellToWorldScale(grid.gridSize);
         size(windowSize.x, windowSize.y);
 
+        // start game timer
         stepDevice = new StepDevice(System.currentTimeMillis(), 250);
     }
 
+    // called every frame of gameplay
     @Override
     public void draw() {
+
+        // step forward in time
         if (stepDevice.setInterval(System.currentTimeMillis())) grid.step();
+
+        // redraw background to black
         background(0);
+
+        // draw game visuals
         grid.draw(this);
     }
 
+    // called when key input press detected
     @Override
     public void keyPressed() {
+
+        // get desired input direction
         Vector dpadInput = dpadMapping.get(key);
+
+        // if direction is valid set player to move in that direction
         if (dpadInput != null) grid.player.moveDirection = dpadInput;
+
+        // make player move on input
         grid.player.moveForce();
     }
 
+    // called when kep input release detected
     @Override
     public void keyReleased() {
+
+        // get released input direction
         Vector dpadInput = dpadMapping.get(key);
+
+        // if released direction is current direction remove the direction
         if (dpadInput != null && dpadInput.equals(grid.player.moveDirection)) grid.player.moveDirection = new Vector();
     }
 }
